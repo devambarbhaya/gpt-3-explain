5d67f465042e3ed329f58b0fd3e843099f04ef69
devambarbhaya <22b2113@iitb.ac.in>
2023-10-06 08:32:47 +0530

    first commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png'
+import dropbox from '../../assets/dropbox.png'
+import shopify from '../../assets/shopify.png'
+import atlassian from '../../assets/atlassian.png'
+import slack from '../../assets/slack.png'
+
+export {
+    google,
+    dropbox,
+    shopify,
+    atlassian,
+    slack
+}
